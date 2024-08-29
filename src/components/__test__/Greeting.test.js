cb8398c83949d929d9bb900d4609a6aa3e54e8ab
AElenaGr <andrae.tandemaranjuez@gmail.com>
2024-08-29 12:07:44 +0200

    sin components y sin paginas

--- src/components/__test__/Greeting.test.js
+++
@@ -1,9 +0,0 @@
-// src/components/__tests__/Greeting.test.js
-import React from "react";
-import { render } from "@testing-library/react";
-import Greeting from "../greeting";
-
-test("muestra un saludo con el nombre correcto", () => {
-  const { getByText } = render(<Greeting name="Gatsby" />);
-  expect(getByText("Hello, Gatsby!")).toBeInTheDocument();
-});
