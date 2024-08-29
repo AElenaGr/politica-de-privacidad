eb2c7c960ca70662a96ae7d552e4c1f695d8d7f6
AElenaGr <andrae.tandemaranjuez@gmail.com>
2024-08-29 12:27:52 +0200

    hola mundo

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+// src/pages/index.js
+import React from 'react';
+import { Link } from 'gatsby';
+import '../styles/index.css';
+
+
+const IndexPage = () => {
+  return (
+    <main>
+      <p>Hola mundo</p>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Patrimonio Nacional</title>;
